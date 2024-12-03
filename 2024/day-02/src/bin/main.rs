0def0888c51644c73e0c5c5c2e815894abc27101
Ana Rodrigues <agdr20@gmail.com>
2024-12-03 08:40:14 +0000

    Complete AoC 2024 day 2 part 2

--- 2024/day-02/src/bin/main.rs
+++ 2024/day-02/src/bin/main.rs
@@ -16,7 +16,7 @@ fn main(){
 
 }
 
-fn is_valid_sequence(numbers: &[i32]) -> bool {
+fn is_sequence_valid(numbers: &[i32]) -> bool {
     if numbers.is_empty() {
         return true;
     }
@@ -43,6 +43,78 @@ fn is_valid_sequence(numbers: &[i32]) -> bool {
     increasing || decreasing
 }
 
+fn are_both_positive_or_both_negative(a: i32, b: Option<i32>) -> bool {
+    match b {
+        Some(b_value) => (a > 0 && b_value > 0) || (a < 0 && b_value < 0),
+        None => true, // Return true if no previous value to compare so we can continue
+    }
+}
+
+fn is_sequence_valid_with_problem_dampener(numbers: &[i32], tolerate: bool) -> bool {
+    if numbers.is_empty() {
+        return true;
+    }
+
+    let mut increasing = true;
+    let mut decreasing = true;
+
+    let mut previous_diff: Option<i32> = None;
+
+    for (index, window) in numbers.windows(2).enumerate() {
+        let diff = window[1] - window[0];
+        // dbg!(&tolerate);
+        // dbg!(&diff);
+        
+        // Check if the difference violates the constraints
+        if (diff.abs() > 3 || diff == 0) || !are_both_positive_or_both_negative(diff, previous_diff) {
+            if !tolerate {
+                return false;
+            }
+
+            // remove one and the other, and if one of them is valid, return true
+            // Create two separate vectors for the two removal cases
+            let mut new_list_removed_current_index = numbers.to_vec();
+            let mut new_list_removed_index_after = numbers.to_vec();
+
+            // Remove the first and second elements in separate vectors
+            new_list_removed_current_index.remove(index);
+            new_list_removed_index_after.remove(index + 1);
+
+            // dbg!(&new_list_removed_current_index);
+            // dbg!(&new_list_removed_index_after);
+
+            let is_first_sequence_valid = is_sequence_valid_with_problem_dampener(&new_list_removed_current_index, false);
+            let is_second_sequence_valid = is_sequence_valid_with_problem_dampener(&new_list_removed_index_after, false);
+
+            // dbg!(is_first_sequence_valid);
+            // dbg!(is_second_sequence_valid);
+
+            let mut is_third_sequence_valid = false;
+            if index > 0 {
+                let mut new_list_removed_index_before = numbers.to_vec();
+                new_list_removed_index_before.remove(index-1);
+                if is_sequence_valid_with_problem_dampener(&new_list_removed_index_before, false) {
+                    is_third_sequence_valid = true;
+                }
+            }
+
+            return is_first_sequence_valid || is_second_sequence_valid || is_third_sequence_valid;
+        }
+
+        // Update the increasing or decreasing status
+        if diff > 0 {
+            decreasing = false;
+        } else {
+            increasing = false;
+        }
+
+        previous_diff = Some(diff);
+
+    }
+
+    increasing || decreasing
+}
+
 
 fn part1(_input: &str) -> String {
 
@@ -57,9 +129,9 @@ fn part1(_input: &str) -> String {
             .map(|s| s.parse::<i32>().unwrap()) // Parse each part to an i32
             .collect();
 
-        dbg!(&parts);
+        // dbg!(&parts);
 
-        are_lines_safe.push(is_valid_sequence(&parts) as i32);
+        are_lines_safe.push(is_sequence_valid(&parts) as i32);
     }
     
     let total_safe: i32 = are_lines_safe.iter().sum();
@@ -69,7 +141,25 @@ fn part1(_input: &str) -> String {
 }
 
 fn part2(_input: &str) -> String {
-    todo!()
+    let mut are_lines_safe: Vec<i32> = Vec::new();
+    // Iterate over lines
+    for line in _input.lines() {
+        // println!("{}", line);
+
+        // Split the line into words by whitespace and collect into a Vec
+        let parts: Vec<i32> = line
+            .split_whitespace() // Split by whitespace
+            .map(|s| s.parse::<i32>().unwrap()) // Parse each part to an i32
+            .collect();
+
+        // dbg!(&parts);
+
+        are_lines_safe.push(is_sequence_valid_with_problem_dampener(&parts, true) as i32);
+    }
+
+    // dbg!(&are_lines_safe);
+    let total_safe: i32 = are_lines_safe.iter().sum();
+    total_safe.to_string()
 }
 
 #[cfg(test)]
@@ -82,7 +172,7 @@ mod tests {
         let expected = 1;
         assert_eq!(result, expected.to_string());
     }
-
+    
     #[test]
     fn test_part1_example() {
         let result = part1("7 6 4 2 1
@@ -95,4 +185,29 @@ mod tests {
         assert_eq!(result, expected.to_string());
     }
 
+    #[test]
+    fn test_part2_one_line() {
+        let result = part2("1 3 2 4 5");
+        let expected = 1;
+        assert_eq!(result, expected.to_string());
+    }
+
+    #[test]
+    fn test_part2_one_line_bug() {
+        let result = part2("1 4 3 2 1");
+        let expected = 1;
+        assert_eq!(result, expected.to_string());
+    }
+        
+    #[test]
+    fn test_part2_example() {
+        let result = part2("7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9");
+        let expected = 4;
+        assert_eq!(result, expected.to_string());
+    }
 }
