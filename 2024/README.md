e19ae9947ec73710838fe940b3d0dc627f8291b2
Ana Rodrigues <agdr20@gmail.com>
2024-12-04 16:54:44 +0000

    Complete AoC 2024 day 3 part 2

--- 2024/README.md
+++ 2024/README.md
@@ -3,4 +3,6 @@
 **Table of contents:**
 
 * [Day 1](/2024/day-01)
+* [Day 2](/2024/day-02)
+* [Day 3](/2024/day-03)
 
--- 2024/day-03/src/bin/main.rs
+++ 2024/day-03/src/bin/main.rs
@@ -61,7 +61,60 @@ fn part1(_input: &str) -> i32 {
 }
 
 fn part2(_input: &str) -> i32 {
-    todo!()
+
+    let mut all_tuples: Vec<(i32, i32)> = Vec::new();
+    // Iterate over lines
+    let mut is_enabled: bool = true;
+    for line in _input.lines() {
+        // println!("{}", line);
+
+        let pattern = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)";
+
+        // Compile the regex
+        let regex = Regex::new(pattern).unwrap();
+
+        // Find all matches
+        let matches: Vec<&str> = regex.find_iter(line).map(|m| m.as_str()).collect();
+        dbg!(&matches);
+        
+        for group in matches {
+            if group == "do()" {
+                is_enabled = true;
+            } else if group == "don't()" {
+                is_enabled = false;
+            } else if is_enabled{
+                let pattern = r"mul\((\d{1,3}),(\d{1,3})\)";
+
+                // Compile the regex
+                let regex = Regex::new(pattern).unwrap();
+
+                // Find all matches
+                let tuple: (i32, i32) = regex
+                .captures_iter(group)
+                .map(|caps| {
+                    let first = caps[1].parse::<i32>().unwrap();
+                    let second = caps[2].parse::<i32>().unwrap();
+                    (first, second)
+                })
+                .next()
+                .unwrap();
+
+                // dbg!(&tuple);
+                all_tuples.push(tuple)
+
+            }
+        }
+
+    }
+    
+    dbg!(&all_tuples);
+
+    // Call the function for each tuple and sum the results
+    let total: i32 = all_tuples.iter().map(|&(a, b)| mul(a, b)).sum();
+
+    // Print the total sum
+    // println!("Total Sum: {}", total);
+    total
 }
 
 #[cfg(test)]
@@ -87,6 +140,13 @@ mod tests {
         let result = part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 ");
         let expected = 161;
+        assert_eq!(result, expected);
+    }
+        
+    #[test]
+    fn test_part2_example() {
+        let result = part2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+        let expected = 48;
         assert_eq!(result, expected);
     }
 }
